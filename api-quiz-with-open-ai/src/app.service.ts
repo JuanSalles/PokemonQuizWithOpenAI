539af0aa63def4b33ee3c912253120ca042445da
Juan Salles <juanprazeres@gmail.com>
2024-02-16 11:36:05 -0300

    Atualizando o projeto de API Nest para receber os Dados JSON da OPENAI

--- api-quiz-with-open-ai/src/app.service.ts
+++ api-quiz-with-open-ai/src/app.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import * as OpenAI from 'openai';
 
 @Injectable()
 export class AppService {
   private openai: OpenAI.OpenAI;
 
-  constructor() {
+  constructor(private configService: ConfigService) {
     this.openai = new OpenAI.OpenAI({
-      apiKey: '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',
+      apiKey: this.configService.get<string>('OPENAI_API_KEY'),
     });
   }
 
@@ -17,7 +18,8 @@ export class AppService {
       messages: [
         {
           role: 'system',
-          content: 'You are a helpful assistant.',
+          content:
+            'You need to answer with a JSON object with 10 questions about programming. The questions should be in Portuguese. The questions must have five options and a correct answer. The options and the correct answer must be in Portuguese. The correct answer must be a letter from A to E. You must follow this model: {questions:[{question: "Exemple of question", options: ["A) option", "B) option", "C) option", "D) option", "E) option"], correct: "A"}]}',
         },
         {
           role: 'user',
@@ -36,10 +38,10 @@ export class AppService {
       const text = response.choices[0].message.content;
       const regex = /\{[^]*\}/;
       const jsonMatch = text.match(regex);
-      // console.log(jsonMatch[0]);
+      console.log(jsonMatch[0]);
       const jsonObject = JSON.parse(jsonMatch[0]);
-      // console.log(jsonObject);
-      return jsonObject.questoes;
+      console.log(jsonObject);
+      return jsonObject;
     } else {
       throw new Error('Resposta inválida da API do OpenAI');
     }
