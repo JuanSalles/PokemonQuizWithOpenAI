0d038f5f24abd03696dde3c1f6e9d5530a227388
Juan Salles <juanprazeres@gmail.com>
2024-02-26 20:16:45 -0300

    Adição do Cors para consumo da API

--- api-quiz-with-open-ai/src/main.ts
+++ api-quiz-with-open-ai/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  app.enableCors({
+    origin: 'https://quizdev.vercel.app', // substitua por seu domínio
+  });
 }
 bootstrap();
